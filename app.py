9d0fe803627e4d4a22068b55f9ff0b288f42313a
Gurunathan <90536937+gurunathan-r@users.noreply.github.com>
2025-10-26 10:12:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -342,4 +342,4 @@ def export_note(note_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
